61de022a66b3cf4b08d09bb54c6b51c59d350430
cirodrig <cirodrig@illinois.edu>
2014-07-27 08:03:56 -0700

    Test transitiveClosure with monotonic functions only
    Ignore-this: 235bcb4d7da68d1d52adf6fced2fffe1
    
    darcs-hash:20140727150356-11156-a9a6d2b602d76605146fcde2debe6288257f06a4

--- CheckBoundedSet.hs
+++ CheckBoundedSet.hs
@@ -1,7 +1,12 @@
 
 module CheckBoundedSet where
 
+import Control.Applicative
+import Control.Monad
+import Data.Maybe
+import qualified Data.Vector as V
 import Test.QuickCheck
+
 import BoundedSet
 
 forAllMembers :: Testable prop => BoundedSet -> (Int -> prop) -> Property
@@ -9,6 +14,39 @@ forAllMembers s p =
   case boundedSetToList s
   of [] -> property True
      xs -> forAll (elements xs) p
+
+-- | Generate a random function from bounded integers to bounded sets.
+--   The function's output is biased toward sets of size @(1 - eps)/bound@.
+--
+--   From graph theory, interpreting the function as giving the edges of a
+--   directed graph, this size is on the border of where the graph has
+--   one giant component.
+genSmallSetFunction :: Int -> Gen (Int -> BoundedSet)
+genSmallSetFunction bound = do
+  -- Precalculate the entire range of the function.
+  range <- replicateM (bound + 1) $ boundedSetFromList <$> gen_set
+  let rangev = V.fromList range
+
+  -- Look up values from the range
+  let f i | i >= 0 && i <= bound = rangev V.! i
+          | otherwise            = error "Index out of bounds"
+  rangev `seq` return f
+  where
+    prob :: Double
+    prob = if bound == 0
+           then 0
+           else (1 - 0.002) / fromIntegral bound
+
+    gen_set :: Gen [Int]
+    gen_set = do
+      liftM catMaybes $ forM [0..bound] $ \i -> do
+        r <- choose (0, 1)
+        return $! if r < prob then Just i else Nothing
+
+genMonotonicBoundedFunction :: Gen (BoundedSet -> BoundedSet)
+genMonotonicBoundedFunction = sized $ \sz -> do
+  f <- genSmallSetFunction sz
+  return $ bsUnions . map f . boundedSetToList
 
 -- Properties that should be tautologies
 singleton_contains x = x `bsMember` bsSingleton x
@@ -36,10 +74,12 @@ difference_includes s t = forAllMembers (bsDifference s t) $ \x ->
 difference_union s t = forAll arbitrary $ \x ->
   x `bsNotMember` bsUnion s t ==> x `bsNotMember` bsDifference s t
 
-closure_contains_f (Blind f) =
+closure_contains_f =
+  forAll (fmap Blind genMonotonicBoundedFunction) $ \(Blind f) ->
   f bsEmpty `bsDifference` bsTransitiveClosure f `bsEqual` bsEmpty
 
-closure_closed (Blind f) =
+closure_closed =
+  forAll (fmap Blind genMonotonicBoundedFunction) $ \(Blind f) ->
   let tc = bsTransitiveClosure f
   in tc `bsEqual` bsUnion tc (f (tc))
 
